b43380422e9307100d388428c6db4ef95fe1fc99
irinadeeva <deeva97@me.com>
2024-09-09 19:42:37 +0300

    Initial Commit

---
+++ Snapsea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snapsea
+//
+//  Created by Irina Deeva on 09/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
